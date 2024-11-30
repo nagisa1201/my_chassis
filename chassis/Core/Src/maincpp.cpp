655e1787469888a68f6d7f7131454e6c87ba4bd8
Nagisa <2964793117@qq.com>
2024-11-30 17:29:05 +0800

    电机库逻辑封装完毕

--- chassis/Core/Src/maincpp.cpp
+++ chassis/Core/Src/maincpp.cpp
@@ -2,14 +2,14 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "string.h"
-extern Motor::MotorInterface_t motor;  
-extern Motor::EncoderInterface_t encoder;
+
+Motor::MotorInterface_t motor(&htim8 , TIM_CHANNEL_1, &htim2 ,28, 13, 300, 0.62 / 2, 5);
 char buffer[50]; 
 void OnceMain();
 
 int main_cpp()
 { 
-    encoder.EncoderpinInit();
+    motor._encoder.EncoderpinInit();
     motor.MotorpinInit();
     return 0;
 }
@@ -22,7 +22,7 @@ void OnceMain()
 void SerialSend()
 {
 
-    sprintf(buffer," %f\n", encoder._velocity);
+    sprintf(buffer," %f\n", motor._encoder._velocity);
     HAL_UART_Transmit(&huart4, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
 
 }
--- chassis/Hardware/motor.h
+++ chassis/Hardware/motor.h
@@ -2,7 +2,7 @@
  * @Author: Nagisa 2964793117@qq.com
  * @Date: 2024-11-09 15:31:14
  * @LastEditors: Nagisa 2964793117@qq.com
- * @LastEditTime: 2024-11-30 15:26:45
+ * @LastEditTime: 2024-11-30 17:26:34
  * @FilePath: \MDK-ARMf:\project\cubemax\chassis\Hardware\motor.h
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -20,6 +20,8 @@
 #define __MOTOR_H
 #include "common.h"
 #include "pid.h"
+#include "Encoder.h"
+class EncoderInterface_t; 
 
 namespace Motor
 {   
@@ -51,14 +53,19 @@ namespace Motor
     {
         public:
             ~MotorInterface_t(){};
-            MotorInterface_t(TIM_HandleTypeDef *htimx, uint32_t Channelx): Motor_base_t(htimx, Channelx){}
+
+             MotorInterface_t(TIM_HandleTypeDef *htimx_pwm, uint32_t Channelx,TIM_HandleTypeDef *htimx_encoder, float reduction_ratio,
+            float encoder_ppr, float e_rpm, float r, float reload_ms): Motor_base_t(htimx_pwm, Channelx), 
+          _encoder(htimx_encoder, reduction_ratio, encoder_ppr, e_rpm, r, reload_ms) {}
+
             void pidControlV(float Target_val);
             void Motor_start();
 
             float _output_velocity; 
             int32_t _output_pulse_v;
-
+            EncoderInterface_t _encoder;
         protected:
+            
             Pid_Incremental_template_t<float, float> _pid = Pid_Incremental_template_t<float, float>({5, 2, 0, -5000, 5000, 2000});
             float _target_val;
             
